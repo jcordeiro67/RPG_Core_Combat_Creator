ab0795094fcd613cdb5866ad5f6ae11e34daa616
John Cordeiro <john@johncordeiro.com>
2020-04-16 22:12:54 -0400

    RPG Combat Creator - Player Basic Movement

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+	private GameObject target;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		target = GameObject.FindGameObjectWithTag ("Player");
+
+	}
+
+	void LateUpdate ()
+	{
+
+		// Exit if no target
+		if (!target) {
+			return;
+		}
+
+		transform.position = target.transform.position;
+
+	}
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour {
+
+	private Vector3 destination;
+	private NavMeshAgent myNavAgent;
+	private Vector3 velocity;
+	private Animator myAnimator;
+
+	void Start ()
+	{
+		myNavAgent = GetComponent<NavMeshAgent> ();
+		destination = myNavAgent.destination;
+		myAnimator = GetComponent<Animator> ();
+
+	}
+
+	void Update ()
+	{
+
+		if (Input.GetMouseButton (0)) {
+
+			MoveToCursor ();
+		}
+		UpdateAnimator ();
+	}
+
+	private void MoveToCursor ()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		bool hasHit = Physics.Raycast (ray, out RaycastHit hit);
+
+		if (hasHit && Vector3.Distance (destination, hit.point) >= myNavAgent.stoppingDistance) {
+			destination = hit.point;
+			myNavAgent.destination = destination;
+		}
+	}
+
+	private void UpdateAnimator ()
+	{
+		velocity = myNavAgent.velocity;
+		Vector3 localVelocity = transform.InverseTransformDirection (velocity);
+		float speed = localVelocity.z;
+		myAnimator.SetFloat ("ForwardSpeed", speed);
+	}
+}
